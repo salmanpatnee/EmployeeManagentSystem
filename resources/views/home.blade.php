c652a842c2df7d91353dfad67f51b7cbbbed2d97
Salman <salmanpatni92@gmail.com>
2022-08-13 12:58:52 +0500

    Add login throttling: 3 failed login attempts will lock user for 5 mins.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.master')
 
 @section('content')
+    <h1 class="mt-4">Dashboard</h1>
+    <ol class="breadcrumb mb-4">
+        <li class="breadcrumb-item active">Dashboard</li>
+    </ol>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -105,10 +105,6 @@
         <div id="layoutSidenav_content">
             <main>
                 <div class="container-fluid px-4">
-                    <h1 class="mt-4">Dashboard</h1>
-                    <ol class="breadcrumb mb-4">
-                        <li class="breadcrumb-item active">Dashboard</li>
-                    </ol>
                     @yield('content')
                 </div>
             </main>
